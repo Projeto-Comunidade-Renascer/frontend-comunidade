be6b67beb17b28c19093cfe58c87b5e7cb58bde6
Isabela Aguiar <isilav.is54@gmail.com>
2026-01-31 20:13:43 -0300

    ✨feat: adicionando a seção 'sobre nossa comunidade' na página inicial

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -10,11 +10,17 @@ function Index() {
 
             <main className="conteudo-principal">
                 <div className="sobre-comunidade">
-                    <h2>Sobre Nossa Comunidade</h2>
-                    <p>Nosso objetivo é criar um espaço acolhedor para compartilhar informações e experiências sobre as dificuldades da gravidez, desafios e dicas da maternidade. Queremos oferecer apoio para que você se sinta ouvida e encorajada a enfrentar os altos e baixos dessa fase tão especial. Além disso, disponibilizamos conteúdos sobre cuidados com a saúde durante a gestação e orientações práticas para o dia a dia com o bebê</p>
-                    <img src={comunidadeImg} alt="grávida" className='comunidade-img' />
+                    <div className="img-comunidade-sec">
+                    <img src={comunidadeImg} alt="grávida" className="comunidade-img" />
+                    </div>
+                    <div className="sec-sobre-nossa-comunidade">
+                        <h2>Sobre Nossa Comunidade</h2>
+                        <p>Nosso objetivo é criar um espaço acolhedor para compartilhar informações e experiências sobre as dificuldades da gravidez, desafios e dicas da maternidade. Queremos oferecer apoio para que você se sinta ouvida e encorajada a enfrentar os altos e baixos dessa fase tão especial. Além disso, disponibilizamos conteúdos sobre cuidados com a saúde durante a gestação e orientações práticas para o dia a dia com o bebê</p>
+                        <button>Participe da comunidade!</button>
+                    </div>
+                    
                 </div>
-                <button>Participe da comunidade!</button>
+                
             </main>
         </>
         )
